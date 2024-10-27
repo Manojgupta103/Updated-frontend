bfa04cd2c73433180e603d0553547d07d04ba693
Aman Singh <amansingh10122003@gmail.com>
2024-10-27 21:48:31 +0530

    Added Maintenance indicator to the unavailable pages

---
+++ DoctorComponents/Unavailable.jsx
@@ -0,0 +1,7 @@
+export default function Unavailable(){
+    return(
+        <div className="w-full h-full bg-white rounded-xl shadow-md items-center flex justify-center">
+            <img src="/doctor/Maintenance.png" alt="" />
+        </div>
+    )
+};
---
+++ app/authenticated/doctor/analytics/page.jsx
@@ -0,0 +1,9 @@
+import Unavailable from "@/DoctorComponents/Unavailable"
+
+export default function Home() {
+  return(
+    <div className="lg:w-4/5 lg:h-4/5 mx-auto lg:p-10 md:p-10 md:h-4/5 md:w-4/5 w-4/5 h-screen">
+        <Unavailable />
+    </div>
+  ) 
+}
